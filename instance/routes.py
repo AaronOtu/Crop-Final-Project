53c88334939002717dc06c0499709a4805eebe97
AaronOtu <aaronotu99@gmail.com>
2024-09-01 20:34:43 -0700

    button disabling

--- instance/routes.py
+++ instance/routes.py
@@ -66,7 +66,7 @@ def login():
 @login_required
 def dashboard():
     greeting = get_greeting()
-    return render_template('dashboard.html', first_name=current_user.first_name, greeting=greeting)
+    return render_template('dashboard.html', first_name=current_user.first_name, greeting=greeting,form_data=request.form)
 
 
 @main.route("/predict", methods=['POST'])
@@ -84,22 +84,13 @@ def predict():
         prediction = model.predict(input_data)
         # single_pred = np.array(feature_list).reshape(1, -1)
 
-        # mx_features = mx.transform(single_pred)
-        # sc_mx_features = sc.transform(mx_features)
-        # prediction = model.predict(sc_mx_features)
-
-        # crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-        #             8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-        #             14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-        #             19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-
         if prediction[0]:
             result = prediction[0]
         else:
             result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
             
         greeting =get_greeting()    
-        return render_template('dashboard.html',result = result, first_name=current_user.first_name, greeting=greeting)
+        return render_template('dashboard.html',result = result, first_name=current_user.first_name, greeting=greeting, form_data=request.form)
 
 
 @main.route('/logout', methods=['GET', 'POST'])
